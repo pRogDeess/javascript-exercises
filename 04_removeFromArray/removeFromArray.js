f8de2faef40dd34ed736de6154a241e3f477af76
D Hills Santillan <dhillsbest2000@gmail.com>
2023-09-24 21:20:06 +0800

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray,...toRemove) {
+    const removedArray = []
+    myArray.forEach((item) => {
+    if(!toRemove.includes(item)){
+        removedArray.push(item)
+    }
+    }); 
+    return removedArray
+    
 };
 
 // Do not edit below this line
